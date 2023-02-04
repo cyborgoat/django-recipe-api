58e56e81af8942716bc2745e835d28b28b11d5fd
cyborgoat <cyborgoat@outlook.com>
2023-02-04 16:25:04 +0800

    added: image models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 """
-Test for models.
+Tests for models.
 """
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -10,12 +11,12 @@ from core import models
 
 
 def create_user(email='user@example.com', password='testpass123'):
-    """Create and return a new user."""
+    """Create a return a new user."""
     return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
-    """Test Models"""
+    """Test models."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
@@ -34,15 +35,15 @@ class ModelTests(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
-    def test_new_user_without_email_raise_error(self):
-        """Test the creating a user without an email raises a ValueError."""
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
 
@@ -57,17 +58,17 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_staff)
 
     def test_create_recipe(self):
-        """Test creating a recipe is successful"""
+        """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
-            'test@exmaple.com',
-            'test123',
+            'test@example.com',
+            'testpass123',
         )
         recipe = models.Recipe.objects.create(
             user=user,
-            title='Sample Recipe Name',
+            title='Sample recipe name',
             time_minutes=5,
             price=Decimal('5.50'),
-            description='Sample Recipe Description',
+            description='Sample receipe description.',
         )
 
         self.assertEqual(str(recipe), recipe.title)
@@ -88,3 +89,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
